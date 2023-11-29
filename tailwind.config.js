4fff1c76080fa1e2f7f4b404661a32f8c80eff7a
thrillim <86657389+thrillim@users.noreply.github.com>
2023-11-29 10:06:05 +0700

    navbar UI

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,4 +1,5 @@
 /** @type {import('tailwindcss').Config} */
+import daisyui from 'daisyui';
 module.exports = {
   content: [
     './pages/**/*.{js,ts,jsx,tsx,mdx}',
@@ -14,5 +15,23 @@ module.exports = {
       },
     },
   },
-  plugins: [],
-}
+  plugins: [daisyui],
+  daisyui: {
+    themes: [
+      {
+        mytheme: {
+          primary: '#55433B',
+          secondary: '#A26952',
+          accent: '#ea580c',
+          neutral: '#030303',
+          'base-100': '#FFE9BD',
+          info: '#0891b2',
+          success: '#047857',
+          warning: '#eab308',
+          error: '#b91c1c',
+        },
+      },
+      'retro',
+    ],
+  },
+};
