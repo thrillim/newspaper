c24340b8722c9e94459c241f9a8dbca870c320af
thrillim <86657389+thrillim@users.noreply.github.com>
2023-12-02 15:29:29 +0700

    add link to newscard

--- components/NewsCard.jsx
+++ components/NewsCard.jsx
@@ -1,11 +1,11 @@
 const NewsCard = (props) => {
   return (
-    <div className="card card-compact w-full bg-base-100 shadow-xl">
+    <a href="/abcs" className="card card-compact w-full bg-base-100 shadow-xl">
       <figure><img src="https://daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.jpg" alt="Shoes" /></figure>
       <div className="card-body">
         <h2 className="card-title text-primary">Shoes! Each child in a list should have a unique "key" prop.</h2>
       </div>
-    </div>
+    </a>
   )
 }
 
