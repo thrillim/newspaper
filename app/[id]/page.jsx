977d672cc2c13600e9cdc69b8ff478974bb2eddc
thrillim <86657389+thrillim@users.noreply.github.com>
2023-12-03 17:52:30 +0700

    update link to fetch

--- app/[id]/page.jsx
+++ app/[id]/page.jsx
@@ -1,5 +1,5 @@
-'use client'
-import NavBar from "../../components/NavBar";
+'use client';
+import NavBar from '../../components/NavBar';
 import NewsCard from '../../components/NewsCard';
 
 // Newspaper page
@@ -9,21 +9,26 @@ const HomePage = ({ params }) => {
   const [articles, setArticles] = useState([]);
   useEffect(() => {
     const fetchArticles = async () => {
-      fetch(`https://newsdata.io/api/1/news?apikey=pub_32853489b697cf2bea5dbc515b878139235d7&country=vi&category=${params.id}`)
-        .then(response => {
+      // fetch(`https://newsdata.io/api/1/news?apikey=pub_32853489b697cf2bea5dbc515b878139235d7&country=vi&category=${params.id}`)
+      fetch(`http://localhost:3002/news?category=${params.id}`)
+        .then((response) => {
           if (!response.ok) {
             throw new Error(`HTTP error! status: ${response.status}`);
           }
           // Then try to parse it as JSON
+          // console.log(response.json())
           return response.json();
         })
-        .then(data => {
+        .then((data) => {
+          console.log(data);
           // Filter out articles without images
-          const filteredArticles = data.results.filter(article => article.image_url !== null);
+          const filteredArticles = data.filter(
+            (article) => article.image_url !== null
+          );
           // Use the filtered data
           setArticles(filteredArticles);
         })
-        .catch(error => {
+        .catch((error) => {
           // Handle the error
           // console.log(error)
         });
@@ -32,10 +37,13 @@ const HomePage = ({ params }) => {
   }, []);
 
   return (
-    <div className="newspaper-page bg-base-100">
-      <div className="p-5 gap-6 grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4">
+    <div className='newspaper-page bg-base-100'>
+      <div className='p-5 gap-6 grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4'>
         {articles.map((article) => (
-          <NewsCard key={article.article_id} article={article} />
+          <NewsCard
+            key={article.article_id}
+            article={article}
+          />
         ))}
       </div>
     </div>
