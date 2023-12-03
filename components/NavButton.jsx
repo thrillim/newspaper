31164c83eff90be4dab4f13b0627cc4a1f190466
thrillim <86657389+thrillim@users.noreply.github.com>
2023-12-03 09:16:43 +0700

    nav-able newspage

--- components/NavButton.jsx
+++ components/NavButton.jsx
@@ -1,7 +1,7 @@
 // import Link from 'next/link'
 
 const NavButton = (props) => {
-  const link=""+props.nav;
+  const link="http://localhost:3000/"+props.nav;
   return props.active ?
     <a href={link} className='btn btn-sm btn-primary text-white'>{props.content}</a> :
     <a href={link} className='btn btn-sm btn-ghost'>{props.content}</a>;
--- components/NewsCard.jsx
+++ components/NewsCard.jsx
@@ -1,6 +1,6 @@
 const NewsCard = (props) => {
   return (
-    <a href="/abcs" className="card card-compact w-full bg-base-100 shadow-xl">
+    <a href="article/6a217d3e796462dc1c95ed6407e1642c" className="card card-compact w-full bg-base-100 shadow-xl border border-primary rounded-md">
       <figure className="h-1/2"><img src={props.article.image_url} alt="none" className="min-h-full min-w-full object-cover"/></figure>
       <div className="card-body">
         <h2 className="card-title text-primary">{props.article.title}</h2>
